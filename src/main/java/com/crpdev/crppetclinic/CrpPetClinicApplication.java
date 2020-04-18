970393bc3349238185e6fbe26207df44f4b61cfa
Rajapandian <rajapandianc@outlook.in>
2020-04-18 15:38:37 +0530

    Initial Project Add

---
+++ src/main/java/com/crpdev/crppetclinic/CrpPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.crpdev.crppetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrpPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrpPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crpdev/crppetclinic/CrpPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crpdev.crppetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrpPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
