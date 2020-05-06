7f0ea775efd9478ff9c3906b84aa32fb1e3a6890
Rajapandian <rajapandianc@outlook.in>
2020-05-06 07:29:55 +0530

    Add PetTypes in with Bootstarp (startup). Closes #32

--- pet-clinic-web/src/main/java/com/crpdev/crppetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/crpdev/crppetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.crpdev.crppetclinic.bootstrap;
 
 import com.crpdev.crppetclinic.model.Owner;
+import com.crpdev.crppetclinic.model.PetType;
 import com.crpdev.crppetclinic.model.Vet;
 import com.crpdev.crppetclinic.services.OwnerService;
+import com.crpdev.crppetclinic.services.PetTypeService;
 import com.crpdev.crppetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,16 +13,28 @@ import org.springframework.stereotype.Component;
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
-
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDog = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCat = petTypeService.save(cat);
+
+        System.out.println("Loaded PetType...");
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Rajapandian");
         owner1.setLastName("Chellimuthu");
