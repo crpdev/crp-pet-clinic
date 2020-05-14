40043c903a3379ad67943eddb4332dd036d5e91e
Rajapandian <rajapandianc@outlook.in>
2020-05-14 07:26:10 +0530

    Convert Vets to JPA Entities. Closes #37

--- pet-clinic-data/src/main/java/com/crpdev/crppetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/crpdev/crppetclinic/model/Specialty.java
@@ -1,7 +1,14 @@
 package com.crpdev.crppetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialty")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/crpdev/crppetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/crpdev/crppetclinic/model/Vet.java
@@ -1,17 +1,23 @@
 package com.crpdev.crppetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
-    private Set<Specialty> specialities = new HashSet<>();
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
+    private Set<Specialty> specialties = new HashSet<>();
 
     public Set<Specialty> getSpecialities() {
-        return specialities;
+        return specialties;
     }
 
     public void setSpecialities(Set<Specialty> specialities) {
-        this.specialities = specialities;
+        this.specialties = specialities;
     }
 }
