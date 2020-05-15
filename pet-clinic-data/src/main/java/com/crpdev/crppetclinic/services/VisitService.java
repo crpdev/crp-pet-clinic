edc84bd49483e36b9764412916d8f2b7e64334c8
Rajapandian <rajapandianc@outlook.in>
2020-05-15 06:46:47 +0530

    Create Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/com/crpdev/crppetclinic/services/VisitService.java
@@ -0,0 +1,12 @@
+package com.crpdev.crppetclinic.services;
+
+import com.crpdev.crppetclinic.model.Visit;
+
+/**
+ * Created by rajapandian
+ * Date: 15/05/20
+ * Project: crp-pet-clinic
+ * Package: com.crpdev.crppetclinic.services
+ **/
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/crpdev/crppetclinic/services/map/VisitMapService.java
@@ -0,0 +1,54 @@
+package com.crpdev.crppetclinic.services.map;
+
+import com.crpdev.crppetclinic.model.Visit;
+import com.crpdev.crppetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by rajapandian
+ * Date: 15/05/20
+ * Project: crp-pet-clinic
+ * Package: com.crpdev.crppetclinic.services.map
+ **/
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    private final VisitService visitService;
+
+    public VisitMapService(VisitService visitService) {
+        this.visitService = visitService;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+
+        if(null == object.getPet() || null == object.getPet().getOwner() || null == object.getPet().getId()
+        || null == object.getPet().getOwner().getId()) {
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
